e8376184da8442ed2b0c6c7747f5bc53c6247f70
hamesjan <jhan31903@gmail.com>
2023-08-17 00:54:58 -0700

    added open ai

--- src/pages/Overview/Overview.js
+++ src/pages/Overview/Overview.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
 import classes from "./Overview.module.css";
 
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+});
+
 const Overview = () => {
-  return <div className="p-8">Hello</div>;
+  const [result, setResult] = useState("");
+
+  const generateImage = async () => {
+    const res = await openai.createImage({
+      prompt: "a cat sitting in a tree",
+      n: 1,
+      size: "512x512",
+    });
+
+    setResult(res.data.data[0].url);
+  };
+
+  return (
+    <div className="p-8">
+      <button onClick={generateImage}>Generate Image</button>
+      {result && <img src={result} alt="Generated" />}
+    </div>
+  );
 };
 
 export default Overview;
